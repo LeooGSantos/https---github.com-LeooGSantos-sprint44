49b8371c9b65fb968ed4035c12f83fb7b3460801
LeooGSantos <rm98912@fiap.com.br>
2023-09-11 09:11:24 -0300

    Estilizando o Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./Menu.css"; // Importe o arquivo de estilo CSS
 
-export default function Menu(){
-
-    return(
-        <nav className="Menu">
-            <Link to='/'>Principal</Link>
-            <span> | </span>
-            <Link to='/Bike'>Bike</Link>
-            <span> | </span>
-            <Link to='/FazerSeguro'>FazerSeguro</Link>
-            <span> | </span>
-            <Link to='/PlanosSeguro'>PlanosSeguro</Link>
-            <span> | </span>
-            <Link to='/RMs'>RMs</Link>
-            <span> | </span>
-
-        </nav>
-    )
+export default function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">Principal</Link>
+      <span> | </span>
+      <Link to="/Bike">Bike</Link>
+      <span> | </span>
+      <Link to="/FazerSeguro">FazerSeguro</Link>
+      <span> | </span>
+      <Link to="/PlanosSeguro">PlanosSeguro</Link>
+      <span> | </span>
+      <Link to="/RMs">RMs</Link>
+    </nav>
+  );
 }
