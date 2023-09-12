defe837aa9bb0c99546759310b4616b54cf69ee8
LeooGSantos <rm98912@fiap.com.br>
2023-09-12 11:30:44 -0300

    Alteração no css da Bike

--- src/routes/Bike/index.jsx
+++ src/routes/Bike/index.jsx
@@ -1,19 +1,24 @@
 import { Link } from 'react-router-dom';
+import './Bike.css'
 
 function Bike() {
   return (
-    <div className="bike-page">
-      <h1>Fazer Seguro</h1>
-      <p>Faça seu seguro agora, de forma dinâmica e rápida!</p>
-      <button>
-        <Link to="/FazerSeguro">Faça já</Link>
-      </button>
+    <div>
+      <div className="quadrado-1">
+        <h1>Fazer Seguro</h1>
+        <p>Faça seu seguro agora, de forma dinâmica e rápida!</p>
+        <button>
+          <Link to="/FazerSeguro">Faça já</Link>
+        </button>
+      </div>
 
-      <h2>Ver Seus Seguros de Bike</h2>
-      <p>Veja seus seguros aqui!</p>
-      <button>
-        <Link to="/PlanosSeguro">Ver seguros</Link>
-      </button>
+      <div className="quadrado-2">
+        <h1>Ver Seguros de Bike</h1>
+        <p>Veja os seguros aqui!</p>
+        <button>
+          <Link to="/PlanosSeguro">Ver seguros</Link>
+        </button>
+      </div>
     </div>
   );
 }
